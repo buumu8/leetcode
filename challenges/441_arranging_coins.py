b35a03f7ab9ae63cb22b5e8f61d589095d82d497
Jettapol Tuetrakul <14010776+buumu8@users.noreply.github.com>
2025-12-23 10:28:14 +0700

    add challenge 441

---
+++ challenges/441_arranging_coins.py
@@ -0,0 +1,41 @@
+''''
+441. Arranging Coins
+
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+ 
+
+Example 1:
+
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+'''
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left, right = 0, n
+
+        while left <= right:
+            mid = (left + right) // 2
+            coins = mid * (mid + 1) // 2
+            if coins == n:
+                return mid
+            elif coins < n:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
