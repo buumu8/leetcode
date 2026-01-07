9993156e6e2dfdd5509211021ce0e09885208c7b
Jettapol Tuetrakul <14010776+buumu8@users.noreply.github.com>
2026-01-07 12:06:57 +0700

    add challenge 4

---
+++ challenges/4_Median_of_two_sorted_arrays.py
@@ -0,0 +1,79 @@
+# 4. Median of Two Sorted Arrays
+
+'''
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+'''
+
+# Solution: Based on merge process of merge sort O(M+N)
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m, n = len(nums1), len(nums2)
+        i = j = 0
+        prev = curr = 0
+
+        for k in range((n + m) // 2 + 1):
+            prev = curr
+            if i < m and (j >= n or nums1[i] <= nums2[j]):
+                curr = nums1[i]
+                i += 1
+            else:
+                curr = nums2[j]
+                j += 1
+        if (n + m) % 2 == 1:
+            return curr
+        else:
+            return (prev + curr) / 2
+
+# Solution: Based on binary search O(log(min(M,N)))
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right:
+            partition1 = (left + right) // 2
+            partition2 = (m + n + 1) // 2 - partition1
+
+            maxLeft1 = float('-inf') if partition1 == 0 else nums1[partition1 - 1]
+            minRight1 = float('inf') if partition1 == m else nums1[partition1]
+
+            maxLeft2 = float('-inf') if partition2 == 0 else nums2[partition2 - 1]
+            minRight2 = float('inf') if partition2 == n else nums2[partition2]
+
+            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2
+                else:
+                    return max(maxLeft1, maxLeft2)
+            elif maxLeft1 > minRight2:
+                right = partition1 - 1
+            else:
+                left = partition1 + 1
